cb2720dddf7151a92d66d3ea7dd9f8865223edec
U-BSD\517459 <517459@SHS-15W2NW3.beaverton.k12.or.us>
2025-09-12 09:30:59 -0700

    there's a board

---
+++ tictactoe.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+char board[4][4] = { {'\0', 'a', 'b', 'c'}
+		     {'1', '\0', '\0', '\0'}
+		     {'2', '\0', '\0', '\0'}
+		     {'3'. '\0', '\0', '\0'} }
+int main()
+{
+  
+  return 0;
+}
+
+void playGame(char board[4][4])
+{
+  
+}
