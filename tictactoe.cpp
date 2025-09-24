baa362ac83d8e9a85a6e165ab0a610cdee18ec78
U-BSD\517459 <517459@SHS-JYBF6X3.beaverton.k12.or.us>
2025-09-24 09:50:11 -0700

    Making sure I pushed everything

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int checkValid(char move[2], int player, char board[4][4]);
-bool checkWin(char board[4][4]);
+int checkWin(char board[4][4]);
 void resetBoard(char cleanBoard[4][4], char board[4][4]);
 void printBoard(char board[4][4]);
 
@@ -138,12 +138,12 @@ int checkValid(char move[2], int player, char board[4][4])
 int checkWin(char board[4][4])
 {
   //Tie
-  bool tie = true
+  bool tie = true;
   for(int i = 1; i < 4; i++)
     {
       for(int j = 1; j < 4; j++)
 	{
-	  if(board[i][j] == '_') { tie = false; }
+	  if(board[i][j] == '\0') { tie = false; }
 	}
     }
   if(tie == true) { return -1; }
